10459acd6689649d3e8e6898dea1cd1e06ae0f22
codelithic <roberto.dirienzo@codelithic.com>
2020-05-08 14:24:38 +0200

    run updated

--- xwinubuntu/run.sh
+++ xwinubuntu/run.sh
@@ -1,11 +1,10 @@
 
-echo "mount point?"
-read mp
 
-echo "docker options?"
-read options
+if [ -z "${DOCKER_PORTS}" ]
+	export DOCKER_PORTS='-p 8888:80 -p 2222:22 '
+fi
 
 
 xhost +si:localuser:root
-docker run -ti  --security-opt="seccomp=unconfined" ${mo} ${options} -e DISPLAY=$DISPLAY -p 8888:80 -p 2222:22  -v /tmp/.X11-unix:/tmp/.X11-unix xclion:1.0
+docker run -ti  --security-opt="seccomp=unconfined" ${DOCKER_MOUNTS} ${DOCKER_OPTIONS} ${DOCKER_PORTS} -e DISPLAY=$DISPLAY  -v /tmp/.X11-unix:/tmp/.X11-unix xclion:1.0
 
