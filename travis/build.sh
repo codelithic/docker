7260e6c531dcbd043fc840935776b61d4d171b11
Roberto  di Rienzo <roberto.dirienzo@terradue.com>
2020-05-11 12:34:38 +0200

    travis script updated

--- travis/build.sh
+++ travis/build.sh
@@ -4,7 +4,7 @@ set -euov pipefail
 
 source travis/libs/variables.sh
 
-export SSH_KEY='ssh-rsa AAAAB3NzaC1yc2EAAAACABEAAACBALJ0Vw927ajlsqoX3CLDNF81Ck4bZm4BSmtrfvKdMe42BeGVy/SilEeG5cAYzHtfN0Hsgxi00JN7wxP5SZnmF+p5T0+o2Zeu4bgLVc718kXNU2hJaAFV06QkNM9z7TFFa7PNpleTGBV6EQPfxjjgjdCGKw+kpv2ROe1hI8xHzwKz'
+export SSH_KEY="${SSH_KEY_PUBLIC}"
 
 sudo docker build --rm -t "${DOCKER_IMAGE}:${DOCKER_TAG}" -f ubuntu/Dockerfile  .  --build-arg  pkey="${SSH_KEY}"  --build-arg user=mrdev  --build-arg bindpath=mct
 
